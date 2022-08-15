8c0ab6477f6adde8bc3e55f08e88f2c44e529f59
SuperMechaDeathChrist <31192003+SuperMechaDeathChrist@users.noreply.github.com>
2022-08-14 20:42:17 -0500

    test whitenoise

--- api/settings.py
+++ api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,11 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
 #STATIC_URL = 'assets/'
+# STATICFILES_DIRS = [
+#     BASE_DIR/ 'polls/templates/',
+#     BASE_DIR/ 'static/',
+#     #BASE_DIR/ 'staticfiles/'
+# ]
+
 STATICFILES_DIRS = [
-    BASE_DIR/ 'polls/templates/',
-    BASE_DIR/ 'static/',
-    #BASE_DIR/ 'staticfiles/'
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
@@ -130,8 +136,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
 
 last_query={
     'series':{},

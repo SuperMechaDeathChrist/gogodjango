53adbec8c45f8214e5d2a0aa1ee541cb05187412
SuperMechaDeathChrist <31192003+SuperMechaDeathChrist@users.noreply.github.com>
2022-08-14 20:26:26 -0500

    test static

--- api/settings.py
+++ api/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'polls/templates',)],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,16 +118,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = 'static/'
-# STATIC_URL = 'assets/'
+#STATIC_URL = 'assets/'
 STATICFILES_DIRS = [
     BASE_DIR/ 'polls/templates/',
+    BASE_DIR/ 'static/',
+    BASE_DIR/ 'staticfiles/'
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
 
 last_query={
     'series':{},
